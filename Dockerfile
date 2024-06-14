f6558f8246238a37d63c26539660c32c2b2649f4
sievers <tiagosievers@gmail.com>
2024-06-14 10:36:41 -0300

    Third Commit

--- Dockerfile
+++ Dockerfile
@@ -44,6 +44,5 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 #
 COPY ./main.py /code/
 
-#
 CMD uvicorn main:app --host 0.0.0.0 --port $PORT
 #CMD ["fastapi", "run", "main.py", "--port", "80"]
--- main.py
+++ main.py
@@ -1,47 +1,52 @@
-from typing import List
+from typing import Union
 from selenium import webdriver
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 app = FastAPI()
-
-def extract_data(element):
-    return {
-        "text": element.text,
-        "attributes": element.get_attribute("class")
-        # Add more attributes as needed
-    }
 
 def download_selenium():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--disable-extensions')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get('https://www.icarros.com.br/principal/index.jsp')
-
     model_input = driver.find_element(By.XPATH, '//*[@id="modelo"]')
     model_input.send_keys("Opala")
-    model_input.send_keys(Keys.ENTER)
-
+    search_button = driver.find_element(By.XPATH, '//*[@id="buscaForm"]/div[2]/button')
+    search_button.send_keys(Keys.ENTER)
     driver.implicitly_wait(10)
-    elements = driver.find_elements(By.XPATH, '//div[contains(@class, "offer-card__header")]')
 
-    data = [extract_data(element) for element in elements]
+    results_header = driver.find_elements(By.XPATH, '//div[contains(@class, "offer-card__header")]')
+    car_names = []
+    car_prices = []
 
-    driver.quit()  # Quit the driver to release resources
-    return data
+    for result_header in results_header:
+        try:
+            car_name = result_header.find_element(By.XPATH,
+                                                  './/p[contains(@class, "label__onLight ids_textStyle_label_medium_bold")]').text
+            car_model = result_header.find_element(By.XPATH,
+                                                   './/p[contains(@class, "label__neutral ids_textStyle_label_xsmall_regular")]').text
+            price_container = result_header.find_element(By.XPATH,
+                                                         '//div[contains(@class, "offer-card__price-container")]')
+            car_price = price_container.find_element(By.XPATH,
+                                                     './/p[contains(@class, "label__onLight ids_textStyle_label_medium_bold")]').text
 
-@app.get("/data", response_model=List[dict])
+            car_names.append(f"{car_name} {car_model}")
+            car_prices.append(car_price)
+
+        except Exception as e:
+            print(f"An error occurred while processing result header: {e}")
+
+    return car_names, car_prices
+
+@app.get("/data")
 async def get_data():
-    return download_selenium()
+    return {"car_name": download_selenium()}
 
 if __name__ == "__main__":
     import uvicorn
